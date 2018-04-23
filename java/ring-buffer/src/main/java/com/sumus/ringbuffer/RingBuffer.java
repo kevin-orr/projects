1dc56a56aecb484af656e66898f83e14639c3316
kevin-orr <kevinorr.home@gmail.com>
2018-04-23 20:42:43 +0100

    added the write protocol

--- java/ring-buffer/src/main/java/com/sumus/ringbuffer/RingBuffer.java
+++ java/ring-buffer/src/main/java/com/sumus/ringbuffer/RingBuffer.java
@@ -7,10 +7,17 @@ package com.sumus.ringbuffer;
 public interface RingBuffer<T> {
 
     /**
-     * Removes the oldest element from the buffer and returns the element.
+     * Removes the oldest element from the ring buffer and returns the element.
      * @return the oldest element in buffer
      * @throws RingBufferIOException when an attempt to read an empty buffer
      */
     T read() throws RingBufferIOException;
-    
+
+    /**
+     * Sticks an element into the ring buffer - the element becomes the first in the ring buffer
+     * @param element what to put in the ring buffer
+     * @throws RingBufferIOException when an attempt is made to write to a full ring buffer
+     */
+    void write(T element) throws RingBufferIOException;
+
 }
