b3a4b4ef8e475f3df6530207c0dc4eb54d6e990b
kevin-orr <kevinorr.home@gmail.com>
2018-04-24 21:36:20 +0100

    add new quese tests

---
+++ java/ring-buffer/src/test/java/RingBufferAsQueueTest.java
@@ -0,0 +1,155 @@
+import com.sumus.ringbuffer.RingBuffer;
+import com.sumus.ringbuffer.RingBufferIOException;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * test Queue type RingBuffer.
+ */
+
+public class RingBufferAsQueueTest {
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    @Test
+    public void readingFromEmptyBufferShouldThrowException() throws RingBufferIOException {
+        RingBuffer<Integer> buffer = RingBuffer.ofTypeAndSize(RingBuffer.Type.Queue, 1);
+
+        expectedException.expect(RingBufferIOException.class);
+        expectedException.expectMessage("Tried to read from empty buffer");
+        buffer.read();
+    }
+
+    @Test
+    public void canReadItemJustWritten() throws RingBufferIOException {
+        RingBuffer<Integer> buffer = RingBuffer.ofTypeAndSize(RingBuffer.Type.Queue, 1);
+
+        buffer.write(1);
+        assertThat(buffer.read(), is(1));
+    }
+
+    @Test
+    public void canReadItemOnlyOnce() throws RingBufferIOException {
+        RingBuffer<Integer> buffer = RingBuffer.ofTypeAndSize(RingBuffer.Type.Queue, 1);
+
+        buffer.write(1);
+        assertThat(buffer.read(), is(1));
+
+        expectedException.expect(RingBufferIOException.class);
+        expectedException.expectMessage("Tried to read from empty buffer");
+        buffer.read();
+    }
+
+    @Test
+    public void readsItemsInOrderWritten() throws RingBufferIOException {
+        RingBuffer<Integer> buffer = RingBuffer.ofTypeAndSize(RingBuffer.Type.Queue, 2);
+
+        buffer.write(1);
+        buffer.write(2);
+        assertThat(buffer.read(), is(1));
+        assertThat(buffer.read(), is(2));
+    }
+
+    @Test
+    public void fullBufferCantBeWrittenTo() throws RingBufferIOException {
+        RingBuffer<Integer> buffer = RingBuffer.ofTypeAndSize(RingBuffer.Type.Queue, 1);
+
+        buffer.write(1);
+        expectedException.expect(RingBufferIOException.class);
+        expectedException.expectMessage("Tried to write to full buffer");
+        buffer.write(2);
+    }
+
+    @Test
+    public void readFreesUpSpaceForWrite() throws RingBufferIOException {
+        RingBuffer<Integer> buffer = RingBuffer.ofTypeAndSize(RingBuffer.Type.Queue, 1);
+
+        buffer.write(1);
+        assertThat(buffer.read(), is(1));
+        buffer.write(2);
+        assertThat(buffer.read(), is(2));
+    }
+
+    @Test
+    public void maintainsReadPositionAcrossWrites() throws RingBufferIOException {
+        RingBuffer<Integer> buffer = RingBuffer.ofTypeAndSize(RingBuffer.Type.Queue, 3);
+
+        buffer.write(1);
+        buffer.write(2);
+        assertThat(buffer.read(), is(1));
+        buffer.write(3);
+        assertThat(buffer.read(), is(2));
+        assertThat(buffer.read(), is(3));
+    }
+
+    @Test
+    public void cantReadClearedItems() throws RingBufferIOException {
+        RingBuffer<Integer> buffer = RingBuffer.ofTypeAndSize(RingBuffer.Type.Queue, 1);
+
+        buffer.write(1);
+        buffer.clear();
+        expectedException.expect(RingBufferIOException.class);
+        expectedException.expectMessage("Tried to read from empty buffer");
+        buffer.read();
+    }
+
+    @Test
+    public void clearFreesUpCapacity() throws RingBufferIOException {
+        RingBuffer<Integer> buffer = RingBuffer.ofTypeAndSize(RingBuffer.Type.Queue, 1);
+
+        buffer.write(1);
+        buffer.clear();
+        buffer.write(2);
+        assertThat(buffer.read(), is(2));
+    }
+
+    @Test
+    public void clearDoesNothingOnEmptyBuffer() throws RingBufferIOException {
+        RingBuffer<Integer> buffer = RingBuffer.ofTypeAndSize(RingBuffer.Type.Queue, 1);
+
+        buffer.clear();
+        buffer.write(1);
+        assertThat(buffer.read(), is(1));
+    }
+
+    @Test
+    public void overwriteActsLikeWriteOnNonFullBuffer() throws RingBufferIOException {
+        RingBuffer<Integer> buffer = RingBuffer.ofTypeAndSize(RingBuffer.Type.Queue, 2);
+
+        buffer.write(1);
+        buffer.overwrite(2);
+        assertThat(buffer.read(), is(1));
+        assertThat(buffer.read(), is(2));
+    }
+
+    @Test
+    public void overwriteRemovesOldestElementOnFullBuffer() throws RingBufferIOException {
+        RingBuffer<Integer> buffer = RingBuffer.ofTypeAndSize(RingBuffer.Type.Queue, 2);
+
+        buffer.write(1);
+        buffer.write(2);
+        buffer.overwrite(3);
+        assertThat(buffer.read(), is(2));
+        assertThat(buffer.read(), is(3));
+    }
+
+    @Test
+    public void overwriteDoesntRemoveAnAlreadyReadElement() throws RingBufferIOException {
+        RingBuffer<Integer> buffer = RingBuffer.ofTypeAndSize(RingBuffer.Type.Queue, 3);
+
+        buffer.write(1);
+        buffer.write(2);
+        buffer.write(3);
+        assertThat(buffer.read(), is(1));
+        buffer.write(4);
+        buffer.overwrite(5);
+        assertThat(buffer.read(), is(3));
+        assertThat(buffer.read(), is(4));
+        assertThat(buffer.read(), is(5));
+    }
+}
